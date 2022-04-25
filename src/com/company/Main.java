f145dceca42647a96849247f8936c4db822eb96e
Пользователь <Guzi_30>
2022-04-26 02:56:04 +0600

    birinchi comit

---
+++ src/com/company/Main.java
@@ -0,0 +1,28 @@
+package com.company;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        String b=scanner.nextLine();
+
+        method(" nurlan ");
+    }
+      static void method(String a){
+          System.out.println(a.toUpperCase());
+          System.out.println(a.trim());
+          System.out.println(a.substring(5));
+      }
+
+
+
+
+//	String[] arr={"Nurlan","Aisha","Asma","Sara"};
+//        for (String a:arr) {
+//            System.out.println(a+ " MEN SENI JAKSHY KOROM!");
+//        }
+
+}
